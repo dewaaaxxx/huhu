9b9ac69ca44d021cb62f89faf6ea0a8ded02d394
xabii666 <lionparcelpangeranderajat@gmail.com>
2026-06-27 20:22:17 +0700

    Update Prediction.fast.h

--- app/src/main/jni/game/inc/Prediction.fast.h
+++ app/src/main/jni/game/inc/Prediction.fast.h
@@ -7,11 +7,11 @@
 #include <Vector/Vectors.h>
 #include <vector>
 
-#include "8bp/Ball.h"
+#include "game/Ball.h"
 
 #include <imgui/inc/persistence.h>
 
-#include "8bp/GameManager.h"
+#include "game/GameManager.h"
 
 static Vec4d table_bounds;
 static bool fastCalc = true;
