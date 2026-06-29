9af1acc85f468de1659fbf330892543b2a4a230b
xabii666 <lionparcelpangeranderajat@gmail.com>
2026-06-30 06:50:43 +0700

    Update menu.h

--- app/src/main/jni/menu.h
+++ app/src/main/jni/menu.h
@@ -484,7 +484,7 @@ INLINE void DrawESP(ImDrawList* draw) {
 
         if (persistent_bool[O("bESP_DrawPocketsShotState")]) {
             for (int i = 0; i < 6; i++) {
-                if (Prediction::pocketStatus[i]) {
+                if (gPrediction->guiData.pocketStatus[i]) {
                     auto screenPos = WorldToScreen(pockets[i]);
                     draw->AddCircle(ImVec2(screenPos.x, screenPos.y), 30, GREEN, 0, 5.f);
                 }
